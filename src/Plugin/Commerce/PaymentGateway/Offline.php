777626933dfa6445b1faedc399cee2c8db9321f5
tavi toporjinschi <vasike@gmail.com>
2016-11-24 13:14:22 +0000

    Implement Offline Payment Gateway.

---
+++ src/Plugin/Commerce/PaymentGateway/Offline.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace Drupal\commerce_cop\Plugin\Commerce\PaymentGateway;
+
+use Drupal\commerce_payment\Entity\PaymentInterface;
+use Drupal\commerce_payment\Entity\PaymentMethodInterface;
+use Drupal\commerce_payment\Exception\HardDeclineException;
+use Drupal\commerce_payment\Exception\InvalidRequestException;
+use Drupal\commerce_payment\PaymentMethodTypeManager;
+use Drupal\commerce_payment\PaymentTypeManager;
+use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OnsitePaymentGatewayBase;
+use Drupal\commerce_price\Price;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides the Offline payment gateway.
+ *
+ * @CommercePaymentGateway(
+ *   id = "offline",
+ *   label = "Offline payment",
+ *   display_label = "Offline payment",
+ *    forms = {
+ *     "add-payment-method" = "Drupal\commerce_cop\PluginForm\Offline\PaymentMethodAddForm",
+ *   },
+ *   payment_method_types = {"offline"},
+ * )
+ */
+class Offline extends OnsitePaymentGatewayBase implements OfflineInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, PaymentTypeManager $payment_type_manager, PaymentMethodTypeManager $payment_method_type_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $payment_type_manager, $payment_method_type_manager);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'description' => '',
+      'information' => [
+        'value' => '',
+        'format' => 'plain_text',
+      ],
+      'checkout' => 1,
+      'terminal' => 1,
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+
+
+    $form['offline'] = [
+      '#type' => 'fieldset',
+      '#title' => t('Offline payment settings'),
+    ];
+    $information_description = $this->t('Information you would like to be shown to users when they select this payment method, such as delivery payment details.');
+    $form['offline']['information'] = [
+      '#type' => 'text_format',
+      '#title' => t('Information'),
+      '#placeholder' => $information_description,
+      '#description' => $information_description,
+      '#default_value' => $this->configuration['information']['value'],
+      '#format' => $this->configuration['information']['format'],
+    ];
+    $form['offline']['description'] = [
+      '#type' => 'textarea',
+      '#title' => t('Description'),
+      '#description' => $this->t('Help text displayed to end user on checkout payment.'),
+      '#placeholder' => $this->t('Help text displayed to end user on checkout payment.'),
+      '#default_value' => $this->configuration['description'],
+    ];
+    $form['offline']['checkout'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Available on checkout'),
+      '#description' => $this->t('TRUE or FALSE indicating whether or not payments can be processed via this payment method through the checkout form.'),
+      '#default_value' => $this->configuration['checkout'],
+    ];
+    $form['offline']['terminal'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Available on terminal'),
+      '#description' => $this->t("TRUE or FALSE indicating whether or not payments can be processed via this payment method through the administrative payment terminal on an order's Payment tab"),
+      '#default_value' => $this->configuration['terminal'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+
+    if (!$form_state->getErrors()) {
+      $form_parents = $form['#parents'];
+      $form_parents[] = 'offline';
+      $values = $form_state->getValue($form_parents);
+      $this->configuration['description'] = $values['description'];
+      $this->configuration['information'] = $values['information'];
+      $this->configuration['checkout'] = $values['checkout'];
+      $this->configuration['terminal'] = $values['terminal'];
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createPayment(PaymentInterface $payment, $capture = FALSE) {
+    if ($payment->getState()->value != 'new') {
+      throw new \InvalidArgumentException('The provided payment is in an invalid state.');
+    }
+    $payment_method = $payment->getPaymentMethod();
+    if (empty($payment_method)) {
+      throw new \InvalidArgumentException('The provided payment has no payment method referenced.');
+    }
+    if (REQUEST_TIME >= $payment_method->getExpiresTime()) {
+      throw new HardDeclineException('The provided payment method has expired');
+    }
+
+    // The remote ID returned by the request.
+    $remote_id = $payment_method->getOwnerId();
+
+    $test = $this->getMode() == 'test';
+    $payment->setTest($test);
+    $payment->state = $capture ? 'capture_completed' : 'authorization';
+    $payment->setRemoteId($remote_id);
+    $payment->setAuthorizedTime(REQUEST_TIME);
+    $payment->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function capturePayment(PaymentInterface $payment, Price $amount = NULL) {
+    if ($payment->getState()->value != 'authorization') {
+      throw new \InvalidArgumentException('Only payments in the "authorization" state can be captured.');
+    }
+    // If not specified, capture the entire amount.
+    $amount = $amount ?: $payment->getAmount();
+
+    $payment->state = 'capture_completed';
+    $payment->setAmount($amount);
+    $payment->setCapturedTime(REQUEST_TIME);
+    $payment->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function voidPayment(PaymentInterface $payment) {
+    if ($payment->getState()->value != 'authorization') {
+      throw new \InvalidArgumentException('Only payments in the "authorization" state can be voided.');
+    }
+
+    $payment->state = 'authorization_voided';
+    $payment->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function refundPayment(PaymentInterface $payment, Price $amount = NULL) {
+    if (!in_array($payment->getState()->value, ['capture_completed', 'capture_partially_refunded'])) {
+      throw new \InvalidArgumentException('Only payments in the "capture_completed" and "capture_partially_refunded" states can be refunded.');
+    }
+    // If not specified, refund the entire amount.
+    $amount = $amount ?: $payment->getAmount();
+
+    // Validate the requested amount.
+    $balance = $payment->getBalance();
+    if ($amount->greaterThan($balance)) {
+      throw new InvalidRequestException(sprintf("Can't refund more than %s.", $balance->__toString()));
+    }
+
+    $old_refunded_amount = $payment->getRefundedAmount();
+    $new_refunded_amount = $old_refunded_amount->add($amount);
+    if ($new_refunded_amount->lessThan($payment->getAmount())) {
+      $payment->state = 'capture_partially_refunded';
+    }
+    else {
+      $payment->state = 'capture_refunded';
+    }
+
+    $payment->setRefundedAmount($new_refunded_amount);
+    $payment->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function createPaymentMethod(PaymentMethodInterface $payment_method, array $payment_details) {
+    // No expected keys required for Offline payments.
+
+    // Non-reusable payment methods usually have an expiration timestamp.
+    $expires = time() + (30 * 24 * 60 * 60);
+    // The remote ID returned by the request.
+    $remote_id = $payment_method->getOwnerId();
+
+    $payment_method->setRemoteId($remote_id);
+    $payment_method->setExpiresTime($expires);
+    $payment_method->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function deletePaymentMethod(PaymentMethodInterface $payment_method) {
+    // Delete the local entity.
+    $payment_method->delete();
+  }
+
+}
